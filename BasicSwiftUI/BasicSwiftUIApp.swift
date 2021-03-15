8c84aa241cc23d3322af88f15df1af931113e0ac
Pablo Erick <pabloprogramador@gmail.com>
2021-03-15 10:54:38 +0000

    Initial Commit

---
+++ BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by Pablo Erick Cardoso on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicSwiftUI
+//
+//  Created by Pablo Erick Cardoso on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
